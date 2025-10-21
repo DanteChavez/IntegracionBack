ca145cb003d75bbc748dfaf0ce63ed88fb42404d
Lainpilled-Hat <lainedhat@gmail.com>
2025-10-21 19:57:24 -0300

    Implementacion HTTPS completada, falta tener un mejor certificado, pero con que tengamos los certificados firmados correctamente, podriamos levantar esto sin problemas

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,16 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // Configuracion de HTTPS
+  const fs = require('fs');
+  const httpsOptions = {    
+    key: fs.readFileSync('./secrets/pulgashopkey.pem'),
+    cert: fs.readFileSync('./secrets/pulgashopcert.pem')
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   
   // Configurar prefijo global para todas las rutas
   app.setGlobalPrefix('api');
@@ -17,7 +26,7 @@ async function bootstrap() {
     .addTag('pagos', 'Endpoints para gestión de pagos')
     .addTag('reembolsos', 'Endpoints para gestión de reembolsos')
     .addTag('webhooks', 'Endpoints para webhooks de proveedores')
-    .addServer('http://localhost:3000', 'Servidor de desarrollo')
+    .addServer('https://localhost:3000', 'Servidor de desarrollo')
     .build();
     
   const document = SwaggerModule.createDocument(app, config);
